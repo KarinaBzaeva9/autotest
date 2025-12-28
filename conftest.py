e6c7e93690b12684145798c40f5bdf22bba8be1f
Багаев Асланбек Ибрагимович <aslanbek.bagaev@sbermegamarket.ru>
2025-12-29 02:40:47 +0300

    fix fix fix fi

--- conftest.py
+++ conftest.py
@@ -5,7 +5,6 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture()
 def browser():
    browser=Options()
-   browser.add_argument("--headless") 
    browser=webdriver.Firefox(options=browser)
    browser.implicitly_wait(10)
    yield browser
