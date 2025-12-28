b006eae3f29b43b9af63beb45f42c0fb7ba3e18e
Багаев Асланбек Ибрагимович <aslanbek.bagaev@sbermegamarket.ru>
2025-12-29 02:39:18 +0300

    fix fix fix fi

--- conftest.py
+++ conftest.py
@@ -6,11 +6,6 @@ from selenium.webdriver.firefox.options import Options
 def browser():
    browser=Options()
    browser.add_argument("--headless") 
-   browser.add_argument("--no-sandbox")
-   browser.add_argument("--disable-dev-shm-usage")
-   browser.set_preference("browser.tabs.remote.autostart", False)
-   browser.set_preference("browser.tabs.remote.autostart.1", False)
-   browser.set_preference("browser.tabs.remote.autostart.2", False)
    browser=webdriver.Firefox(options=browser)
    browser.implicitly_wait(10)
    yield browser
